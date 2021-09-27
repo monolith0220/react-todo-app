43ccc522b20a97ba15f683b26a2afca87371c7c7
monolith0220 <63141976+monolith0220@users.noreply.github.com>
2021-09-28 00:00:58 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div></div>
+    </>
+  );
+};
